48352299cf64270f59abc164a5ad1fc9b334c410
mikolajmichalczyk <miko20020921@gmail.com>
2023-12-14 16:18:32 +0100

    team screen

--- src/main/java/mikolaj/project/backendapp/repo/MemberRepo.java
+++ src/main/java/mikolaj/project/backendapp/repo/MemberRepo.java
@@ -2,11 +2,15 @@ package mikolaj.project.backendapp.repo;
 
 
 import mikolaj.project.backendapp.model.Member;
+import mikolaj.project.backendapp.model.Team;
 import mikolaj.project.backendapp.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepo extends RepoTemplate<Member>{
     Optional<Member> findMemberByUserId(Integer userId);
     Optional<Member> findMemberByUser(User user);
+
+    List<Member> findAllByTeam(Team team);
 }
--- src/main/java/mikolaj/project/backendapp/repo/SQL/MemberRepoSQL.java
+++ src/main/java/mikolaj/project/backendapp/repo/SQL/MemberRepoSQL.java
@@ -2,11 +2,13 @@ package mikolaj.project.backendapp.repo.SQL;
 
 
 import mikolaj.project.backendapp.model.Member;
+import mikolaj.project.backendapp.model.Team;
 import mikolaj.project.backendapp.model.User;
 import mikolaj.project.backendapp.repo.MemberRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -17,4 +19,7 @@ public interface MemberRepoSQL extends JpaRepository<Member, Long>, MemberRepo {
 
     @Override
     Optional<Member> findMemberByUser(User user);
+
+    @Override
+    List<Member> findAllByTeam(Team team);
 }
