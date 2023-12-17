1b5250d7ccd27de903b8158515a4c09e20fcfd8a
mikolajmichalczyk <miko20020921@gmail.com>
2023-12-17 19:47:40 +0100

    enter screen and registration

--- src/main/java/mikolaj/project/backendapp/repo/SQL/TrainerRepoSQL.java
+++ src/main/java/mikolaj/project/backendapp/repo/SQL/TrainerRepoSQL.java
@@ -1,11 +1,13 @@
 package mikolaj.project.backendapp.repo.SQL;
 
+import mikolaj.project.backendapp.model.Team;
 import mikolaj.project.backendapp.model.Trainer;
 import mikolaj.project.backendapp.model.User;
 import mikolaj.project.backendapp.repo.TrainerRepo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +16,7 @@ public interface TrainerRepoSQL extends JpaRepository<Trainer, Long>, TrainerRep
     Optional<Trainer> findTrainerByUserId(Integer userId);
     @Override
     Optional<Trainer> findTrainerByUser(User user);
+
+    @Override
+    List<Trainer> findAllByTeam(Team team);
 }
--- src/main/java/mikolaj/project/backendapp/repo/TrainerRepo.java
+++ src/main/java/mikolaj/project/backendapp/repo/TrainerRepo.java
@@ -1,13 +1,16 @@
 package mikolaj.project.backendapp.repo;
 
 
+import mikolaj.project.backendapp.model.Team;
 import mikolaj.project.backendapp.model.Trainer;
 import mikolaj.project.backendapp.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TrainerRepo extends RepoTemplate<Trainer>{
     Optional<Trainer> findTrainerByUserId(Integer userId);
 
     Optional<Trainer> findTrainerByUser(User user);
+    List<Trainer> findAllByTeam(Team team);
 }
